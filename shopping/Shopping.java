20b525098a9f0b0730bf66d1f6478b1976fcf487
BrandonRix <31491902+BrandonRix@users.noreply.github.com>
2017-11-07 14:36:35 -0500

    Created Shop Item inner class

--- shopping/Shopping.java
+++ shopping/Shopping.java
@@ -10,6 +10,26 @@ public class Shopping {
 		catch(InterruptedException ie){
 			ie.printStackTrace();
 		}
+		
+		// Creates a shop item, specifying the item and its price
+		ShopItem potion = new ShopItem(new Item(ItemType.healthPotion), 50);
 	}
 
+	// private inner shop item class , should create a purchase method, handling
+	// what happens if player
+	// does not have enough coins to buy the item
+	private class ShopItem {
+		Item item;
+		int price;
+
+		public ShopItem(Item item, int price) {
+			this.item = item;
+			this.price = price;
+		}
+
+		public void buy() {
+			// subtract gold
+			// add to player inventory
+		}
+	}
 }
